ee5de0958ed2546b51f5e03266d9faa2c666cf2d
Chandruprakasam <pchandru252@gmail.com>
2021-12-07 20:24:52 +0530

    PasssWord Functionality Completed

--- src/org/login/Logo.java
+++ src/org/login/Logo.java
@@ -2,4 +2,11 @@ package org.login;
 
 public class Logo {
 
+	private static void passWord() {
+		System.out.println("password Functionality");
+	}
+	
+	public static void main(String[] args) {
+		passWord();
+	}
 }
